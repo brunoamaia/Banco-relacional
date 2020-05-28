e1418c1e473752c02298353865a44d00128c4249
brunoamaia <brunoamaia94@gmail.com>
2020-05-28 14:31:46 -0300

    Criar banco de dados

---
+++ CriarDatabase.sql
@@ -0,0 +1,4 @@
+-- Operações com Database
+-- Execute apenas a linha desejada (Ctrl+Shift+E)
+create database base_teste;     -- Criar um Database
+drop database base_teste;       -- Apagar um Database
---
+++ CriarTabelaEstado.sql
@@ -0,0 +1,12 @@
+-- Comentários no arquivo de Dados
+-- Criando a tabela Estado 
+create table estados (  -- Definir as colunas e as regras relacionadas as colunas
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+    sigla VARCHAR(2) NOT NULL,
+    regiao ENUM('Norte', 'Nordeste', 'Centro-Oeste', 'Sudeste', 'Sul') NOT NULL, -- Obria que seja um dos valores em ENUM
+    populacal DECIMAL(5,2) NOT NULL,    -- 5 digitos, sendo 2, flutuantes
+    PRIMARY KEY (id),                   -- Para ter chave primaria com 2 colunsa, basta colocar (a,b)
+    UNIQUE KEY (nome),
+    UNIQUE KEY (sigla)
+);
