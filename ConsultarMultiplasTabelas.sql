a5a49ded98cc0ec80efe6fdf0b60a7ebbccda279
brunoamaia <brunoamaia94@gmail.com>
2020-05-29 17:21:49 -0300

    Update ConsultarMultiplasTabelas.sql

--- ConsultarMultiplasTabelas.sql
+++ ConsultarMultiplasTabelas.sql
@@ -1,4 +1,11 @@
 -- Seleciona todos os dados de todas as tabelas 
--- ** No VSCode não apresenta todas (mostra apenas o primeiro ID),  mas no workbenck funciona
+-- ** No VSCode não apresenta todas (mostra apenas o primeiro ID e NOME),  mas no workbenck funciona
 SELECT * FROM estados, cidades
 WHERE estados.id = cidades.estado_id;
+
+SELECT 
+    c.nome as Cidade,
+    e.nome as Estado,
+    regiao as Região
+FROM estados e, cidades c       -- Dar uma "apelido/ abreviação" para as tabelas, ajuda reduzir os comandos
+WHERE c.estado_id = e.id;
